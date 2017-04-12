5eae7b3c58929139d95cc175868cd89dbd8aa805
Ben Hancock <hancocb@purdue.edu>
2017-04-12 11:47:14 -0400

    add progress bar to upload

--- static_storage/js/s3upload/s3-upload.js
+++ static_storage/js/s3upload/s3-upload.js
@@ -20,16 +20,27 @@ function uploadFile (file, s3Data, url) {
   var xhr = new XMLHttpRequest();
   xhr.open("POST", s3Data.url);
 
+  $(".progress").addClass("progress-active");
+
   var postData = new FormData();
   for (key in s3Data.fields) {
     postData.append(key, s3Data.fields[key]);
   }
   postData.append("file", file);
 
+  xhr.upload.onprogress = function(data) {
+    progressBar(data, true);
+  };
+
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
       if (xhr.status === 200 || xhr.status === 204) {
         // do something with the s3 file url
+        setTimeout(function () {
+          $(".progress").removeClass("progress-active");
+          var bar = document.querySelector(".bar");
+          bar.style.width = "0%"
+        }, 2000);
       } else {
         alert("Could not upload file.");
       }
@@ -37,3 +48,12 @@ function uploadFile (file, s3Data, url) {
   };
   xhr.send(postData);
 }
+
+var progressBar = function(data, showProgress) {
+  if (data.lengthComputable === false || showProgress === false) return;
+
+  var pcnt = Math.round(data.loaded * 100 / data.total),
+      bar = document.querySelector(".bar");
+
+  bar.style.width = pcnt + "%"
+};
