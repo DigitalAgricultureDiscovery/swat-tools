2551eeec333a8208ccc0d47c72b82eaf38b66850
Ben Hancock <hancocb@purdue.edu>
2017-04-12 13:22:35 -0400

    create model to track files uploaded to s3

--- s3upload/models.py
+++ s3upload/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class UserTask(models.Model):
+    """ Keep track of files uploaded to S3 bucket. """
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    email = models.EmailField()
+    task_id = models.CharField(max_length=100)
+    type = models.CharField(max_length=2)
+    s3_url = models.CharField(max_length=255)
+    time_started = models.DateTimeField(auto_now_add=True)
