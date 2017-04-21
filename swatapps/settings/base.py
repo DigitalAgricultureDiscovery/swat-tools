80ba0216fb1bd66884eade4412606a47dd9e9009
Ben Hancock <hancocb@purdue.edu>
2017-04-21 16:51:55 -0400

    change redis server address and fix bug with s3 upload code

--- swatapps/settings/base.py
+++ swatapps/settings/base.py
@@ -88,7 +88,7 @@ DATABASES = {
 }
 
 # Celery
-BROKER_URL = 'redis://localhost:6379/0'
+BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
