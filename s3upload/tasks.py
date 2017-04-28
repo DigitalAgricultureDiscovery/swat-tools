c7fe93d46f710c737a44c3caebb1fa28d1a8377a
Ben Hancock <hancocb@purdue.edu>
2017-04-28 10:11:56 -0400

    add more handlers and loggers to separate tasks

--- s3upload/tasks.py
+++ s3upload/tasks.py
@@ -1,14 +1,29 @@
 from __future__ import absolute_import
 
-from django.utils import timezone
-from .models import S3Upload
 from celery import shared_task
+from celery.utils.log import get_task_logger
+from django.utils import timezone
+
+from .models import S3Upload
+
+
+logger = get_task_logger(__name__)
 
 
 @shared_task
 def clean_up_database():
+
+    logger.info("Starting clean up database task.")
+
     # Get date from exactly two dates ago
     two_days_ago = timezone.datetime.now() - timezone.timedelta(days=2)
 
-    # Remove objects 48 hours or older in S3Upload database table
-    S3Upload.objects.filter(time_uploaded__lte=two_days_ago).delete()
+    logger.info("Created timestamp for exactly two days from now.")
+
+    try:
+        # Remove objects 48 hours or older in S3Upload database table
+        S3Upload.objects.filter(time_uploaded__lte=two_days_ago).delete()
+
+        logger.info("Expired records (if any) removed from database.")
+    except:
+        logger.error("Failed to delete expired records from database.")
