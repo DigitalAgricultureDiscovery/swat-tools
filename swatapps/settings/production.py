c6feb9c752eb68dd2c53eba956cbfb76168c613c
Ben Hancock <hancocb@purdue.edu>
2018-10-03 09:34:51 -0400

    update to production settings

--- swatapps/settings/production.py
+++ swatapps/settings/production.py
@@ -15,10 +15,10 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['precision.ag.purdue.edu']
+ALLOWED_HOSTS = ['saraswat-swat.rcac.purdue.edu']
 
+CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_AGE = 1209600  # Two weeks
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_SAVE_EVERY_REQUEST = True
-CSRF_COOKIE_SECURE = True
