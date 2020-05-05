d13c56ef76b532a0bbc6e1a8339642318512f3b5
Ben Hancock <hancocb@purdue.edu>
2020-05-05 10:13:01 -0400

    create a few tests for SWATModelZip class

--- common/SWATModelZip.py
+++ common/SWATModelZip.py
@@ -9,11 +9,18 @@ upload = {
     "on_s3": {"url": "http://"}
 }
 """
+UPLOAD_KEYS = ["cwd", "filename", "on_s3"]
+
+
+class NotAZipError(Exception):
+    pass
 
 
 class SWATModelZip:
 
     def __init__(self, upload: dict) -> None:
+
+        check_upload_keys(upload)
 
         self.model = f"{upload['cwd']}/{os.path.splitext(upload['filename'][0])}"
         self.valid_model = {
@@ -22,7 +29,8 @@ class SWATModelZip:
             'hrus': False        # hru files in TxtInOut
         }
 
-        remove_existing_upload(upload["cwd"], upload["filename"])
+        if not os.environ.get("TEST_FLAG"):
+            remove_existing_upload(upload["cwd"], upload["filename"])
 
         if upload["on_s3"]:
             download_file_to_server(upload["cwd"], upload["on_s3"]["url"])
@@ -37,6 +45,21 @@ class SWATModelZip:
             "status": 0,
             "errors": []
         }
+
+
+def check_upload_keys(upload: dict) -> None:
+    required_keys_included = [key in upload.keys() for key in UPLOAD_KEYS]
+    if not all(required_keys_included):
+        missing_required_keys = ""
+        for index, value in enumerate(required_keys_included):
+            if value is False:
+                if missing_required_keys:
+                    missing_required_keys += f', "{UPLOAD_KEYS[index]}"'
+                else:
+                    missing_required_keys += f', "{UPLOAD_KEYS[index]}"'
+
+        raise KeyError(
+            f'Parameter "upload" dictionary missing key(s): {missing_required_keys}.')
 
 
 def remove_existing_upload(cwd: str, filename: str) -> None:
@@ -96,7 +119,10 @@ def check_if_file_exists(filepath: str) -> bool:
     bool
         True if the file exists, false if it does not.
     """
-    return os.path.exists(filepath)
+    if not os.path.exists(filepath):
+        raise FileNotFoundError("SWAT Model zip not in specified directory")
+    else:
+        return True
 
 
 def check_if_file_is_zip(filepath: str) -> bool:
@@ -113,7 +139,8 @@ def check_if_file_is_zip(filepath: str) -> bool:
     bool
         True if the file has a zip extension, false if it does not.
     """
-    return os.path.splitext(filepath)[1] == '.zip'
+    if not os.path.splitext(filepath)[1] == '.zip':
+        raise NotAZipError("Uploaded file does not have the .zip extension")
 
 
 def unzip_file(cwd: str, filename: str) -> None:
