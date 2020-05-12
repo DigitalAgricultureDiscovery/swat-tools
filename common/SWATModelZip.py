4ba98b7f4b9dec410ec1727b72f72f0cb73d2e1c
Ben Hancock <hancocb@purdue.edu>
2020-05-11 21:26:16 -0400

    add check for hru_id field in swat model hru1 shapefile, add test for new check

--- common/SWATModelZip.py
+++ common/SWATModelZip.py
@@ -4,6 +4,7 @@ import shutil
 import subprocess
 
 from django.core.files.uploadedfile import UploadedFile
+import shapefile
 
 from .utils import create_working_directory, fix_file_permissions
 
@@ -21,7 +22,8 @@ class SWATModelZip:
             "folders": False,    # model directory containing "Scenarios" and "Watershed"
             "raster": False,     # hrus1 raster grid
             "shapefile": False,  # hru1 shapefile
-            "hrus": False        # hru files in TxtInOut
+            "hrus": False,       # hru files in TxtInOut
+            "hru_id": False,     # HRU_ID field present in hru1 shapefile
         }
         self.swat_model_directory = ""
 
@@ -73,7 +75,13 @@ class SWATModelZip:
         self.errors["shapefile"] = check_for_hru1_shp(
             self.swat_model_directory)
         self.errors["hrus"] = check_for_hrus(self.swat_model_directory)
-        
+
+        if self.errors["shapefile"]:
+            self.errors["hru_id"] = check_for_hru_id_field(
+                self.swat_model_directory)
+        else:
+            self.errors["hru_id"] = True
+
         # Run check only for Field SWAT
         if tool == "field":
             self.errors["swatmdb"] = check_for_swatoutput_mdb(
@@ -380,6 +388,30 @@ def check_for_swatoutput_mdb(model_directory: str) -> bool:
         "TablesOut",
         "SWATOutput.mdb"
     ))
+
+
+def check_for_hru_id_field(model_directory: str) -> bool:
+    """
+    Checks for the HRU_ID field in hru1 shapefile.
+
+    Parameters
+    ----------
+    model_directory: str
+        Current working directory for the SWAT model.
+
+    Returns
+    -------
+    bool
+        True if HRU_ID field found, False otherwise.
+    """
+    hru1_shp = os.path.join(model_directory, "Watershed", "Shapes", "hru1.shp")
+    sf = shapefile.Reader(hru1_shp)
+
+    for field in sf.fields:
+        if field[0].lower() == "hru_id":
+            return True
+
+    return False
 
 
 def get_error_message(error: str, model_directory: str = None) -> str:
--- tests/test_SWATModelZip.py
+++ tests/test_SWATModelZip.py
@@ -19,6 +19,7 @@ class TestSWATModelZip(unittest.TestCase):
         self.swat_model_missing_root = "./tests/data/SWAT_Model_No_Root_Folder.zip"
         self.swat_model_missing_swat_folders = "./tests/data/SWAT_Model_Missing_SWAT_Folders.zip"
         self.swat_model_missing_swatoutput_database = "./tests/data/SWAT_Model_Missing_SWATOutput_database.zip"
+        self.swat_model_missing_hru_id_field = "./tests/data/SWAT_Model_Missing_HRU_ID_Field.zip"
 
         if os.path.exists(self.workspace):
             shutil.rmtree(self.workspace)
@@ -35,6 +36,8 @@ class TestSWATModelZip(unittest.TestCase):
         shutil.copy(self.swat_model_missing_swat_folders,
                     os.path.join(self.workspace, "input"))
         shutil.copy(self.swat_model_missing_swatoutput_database,
+                    os.path.join(self.workspace, "input"))
+        shutil.copy(self.swat_model_missing_hru_id_field,
                     os.path.join(self.workspace, "input"))
 
     def tearDown(self):
@@ -167,3 +170,20 @@ class TestSWATModelZip(unittest.TestCase):
 
         self.assertEqual(validation_results["status"], 1)
         self.assertFalse(model.errors["swatmdb"])
+
+    def test_missing_hru_id_field_detected(self):
+        """
+        Test that the appropriate error is thrown when the 
+        hru1 shapefile is missing a field named "HRU_ID".
+        """
+        upload = {
+            "workspace": self.workspace,
+            "local": UploadedFile(name="SWAT_Model_Missing_HRU_ID_Field.zip"),
+            "aws": {}
+        }
+
+        model = SWATModelZip(upload)
+        validation_results = model.validate_model()
+
+        self.assertEqual(validation_results["status"], 1)
+        self.assertFalse(model.errors["hru_id"])
